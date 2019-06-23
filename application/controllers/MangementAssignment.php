7b5b2f70194204ad0b757d282aba69a9fa6b9e21
Arseno Feri Alzahabi <arsenoferi@hotmail.com>
2019-06-23 15:06:15 +0700

    coba commit m

--- application/controllers/MangementAssignment.php
+++ application/controllers/MangementAssignment.php
@@ -1,1 +1,17 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
+class ManagementAssigment extends CI_Controller {
+	
+	
+	public function __construct()
+	{
+		parent::__construct();
+        $this->load->library('seno');
+        $this->load->model('User_model');
+    }
+
+    
+    }
+    
+}
