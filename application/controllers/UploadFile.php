64288c302155fd11d32e2d462da25aeaebbde8bf
Arseno Feri Alzahabi <arsenoferi@hotmail.com>
2019-10-06 22:31:46 +0700

    1

---
+++ application/controllers/UploadFile.php
@@ -0,0 +1,25 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+
+
+class UploadFile extends CI_Controller {
+
+	
+
+	
+
+	public function __construct()
+
+	{
+
+		parent::__construct();
+
+        $this->load->library('seno');
+
+        $this->load->model('User_model');
+
+    }
+}
+?>
